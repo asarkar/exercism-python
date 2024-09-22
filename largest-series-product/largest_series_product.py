3a109d533a8705c5fff50b16669560af232d4326
Abhijit Sarkar <contact@asarkar.com>
2024-09-22 09:35:35 -0700

    Simplify largest-series-product

--- largest-series-product/largest_series_product.py
+++ largest-series-product/largest_series_product.py
@@ -1,9 +1,7 @@
 # To avoid repeated multiplications, we maintain a running product.
 # Whenever we exceed the length of a span, we divide the product
 # by the outgoing number, provided the outgoing number is not zero.
-# We also keep track of the index of the last zero, because as long
-# as there is a zero in the span, the product is zero. We restart
-# the product when we get past a zero.
+# We restart the product when we get past a zero.
 def largest_product(txt: str, span: int) -> int:
     if span > len(txt):
         raise ValueError("span must be smaller than string length")
@@ -11,25 +9,22 @@ def largest_product(txt: str, span: int) -> int:
         raise ValueError("span must not be negative")
 
     pdt = 1
-    max_pdt = start = 0
-    last_index_of_zero = -1
+    cnt = max_pdt = 0
 
-    for i, ch in enumerate(txt):
+    for end, ch in enumerate(txt):
         if not ch.isdigit():
             raise ValueError("digits input must only contain digits")
-        k = int(ch)
-        if k == 0:
-            last_index_of_zero = i
-        if i > 0 and last_index_of_zero == (i - 1):
-            pdt = k
-        else:
-            pdt *= k
 
-        if (i - start + 1) == span:
-            if last_index_of_zero < start:
-                max_pdt = max(max_pdt, pdt)
-                if (x := int(txt[start])) != 0:
-                    pdt //= x
-            start += 1
+        k = int(ch)
+        pdt *= k
+        cnt += 1
+
+        if k == 0:
+            pdt = 1
+            cnt = 0
+        elif cnt >= span:
+            if end >= span and (prev := txt[end - span]) != "0":
+                pdt //= int(prev)
+            max_pdt = max(pdt, max_pdt)
 
     return max_pdt
