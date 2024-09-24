82b403361afdc999cd3ac322b1778b5648f56fb0
Abhijit Sarkar <contact@asarkar.com>
2024-09-23 22:09:48 -0700

    Simplify palindrome-products

--- README.md
+++ README.md
@@ -9,6 +9,13 @@ Feel free to open issues for questions, comments, or suggestions.
 ./.github/run.sh <directory>
 ```
 
+## Timing code
+```
+palindrome_product% ../venv/bin/python -m timeit \
+    -s "from palindrome_products import largest" \
+    "largest(min_factor=100, max_factor=999)"   
+```
+
 ## License
 
 Released under [Apache License v2.0](LICENSE).
--- palindrome-products/palindrome_products.py
+++ palindrome-products/palindrome_products.py
@@ -11,7 +11,7 @@ def largest(min_factor: int, max_factor: int) -> tuple[int | None, list[list[int
     :return: tuple of (palindrome, iterable).
              Iterable should contain both factors of the palindrome in an arbitrary order.
     """
-    return __palindrome(range(max_factor, min_factor - 1, -1), operator.ge)
+    return _palindrome(range(max_factor, min_factor - 1, -1), operator.ge)
 
 
 def smallest(min_factor: int, max_factor: int) -> tuple[int | None, list[list[int]]]:
@@ -23,10 +23,10 @@ def smallest(min_factor: int, max_factor: int) -> tuple[int | None, list[list[in
     :return: tuple of (palindrome, iterable).
     Iterable should contain both factors of the palindrome in an arbitrary order.
     """
-    return __palindrome(range(min_factor, max_factor + 1), operator.le)
+    return _palindrome(range(min_factor, max_factor + 1), operator.le)
 
 
-def __palindrome(r: range, op: Callable[[int, int], bool]) -> tuple[int | None, list[list[int]]]:
+def _palindrome(r: range, op: Callable[[int, int], bool]) -> tuple[int | None, list[list[int]]]:
     """
     Searches for a palindrome with factors in the given range. Whether the palindrome is the
     maximum or minimum depends on the given operator.
@@ -39,6 +39,7 @@ def __palindrome(r: range, op: Callable[[int, int], bool]) -> tuple[int | None, 
         raise ValueError("min must be <= max")
     palindrome: int | None = None
     factors: list[list[int]] = []
+
     for left in r:
         # This variable determines whether the last iterations of the inner loop
         # produced a product that satisfied the given condition when compared with
@@ -49,8 +50,8 @@ def __palindrome(r: range, op: Callable[[int, int], bool]) -> tuple[int | None, 
         # One factor is smaller or equal to the other.
         for right in range(r.start, left + r.step, r.step):
             pdt = left * right
-            if palindrome and not op(pdt, palindrome):
-                continue
+            if palindrome is not None and not op(pdt, palindrome):
+                break
             should_continue = True
             x = str(pdt)
             if x != x[::-1]:
@@ -64,4 +65,5 @@ def __palindrome(r: range, op: Callable[[int, int], bool]) -> tuple[int | None, 
             factors.append([left, right])
         if not should_continue:
             break
+
     return palindrome, factors
