f08f3ef5d081445e9ebc0276290496b74733e881
Abhijit Sarkar <contact@asarkar.com>
2025-10-25 15:02:54 -0700

    Simplify clock

--- clock/clock.py
+++ clock/clock.py
@@ -3,29 +3,35 @@ from __future__ import annotations
 
 class Clock:
     def __init__(self, hour: int, minute: int) -> None:
-        hr = hour % 24
-        (h, m) = divmod(minute, 60)
-        x = (hr + h) % 24
-        self.hour = Clock.__normalize(x, 24)
-        self.minutes = Clock.__normalize(m, 60)
-        self.hr = hour
-        self.min = minute
-
-    @staticmethod
-    def __normalize(x: int, base: int) -> int:
-        return (base - x) if x < 0 else x
+        # `//` returns the Euclidean quotient of `a` divided by `b`.
+        # It performs floor division, rounding the quotient toward negative infinity.
+        # Examples:
+        # -40 // -60 =  0
+        #  40 // -60 = -1
+        # -40 // 60  = -1
+        #  40 // 60  =  0
+        hour = (hour + (minute // 60)) % 24
+        self.hours = hour
+        # `%` returns the Euclidean remainder of `a` divided by `b`.
+        # The result always has the same sign as the divisor `b`.
+        # Examples:
+        # -40 % -60 = -40
+        #  40 % -60 = -20
+        # -40 % 60  =  20
+        #  40 % 60  = 40
+        self.minutes = minute % 60
 
     def __repr__(self) -> str:
-        return f"Clock({self.hr}, {self.min})"
+        return f"Clock({self.hours}, {self.minutes})"
 
     def __str__(self) -> str:
-        return f"{self.hour:02}:{self.minutes:02}"
+        return f"{self.hours:02}:{self.minutes:02}"
 
     def __eq__(self, other: object) -> bool:
         return isinstance(other, Clock) and str(self) == str(other)
 
     def __add__(self, minutes: int) -> Clock:
-        return Clock(self.hour, self.minutes + minutes)
+        return Clock(self.hours, self.minutes + minutes)
 
     def __sub__(self, minutes: int) -> Clock:
-        return self.__add__(-minutes)
+        return Clock(self.hours, self.minutes - minutes)
